2ded8c88f3432b29c38ec7b04bb84c67696dc513
KEN-4 <cuuw1561@icloud.com>
2023-08-06 18:46:51 +0900

    add logout button

--- lib/views/main/profile_screen.dart
+++ lib/views/main/profile_screen.dart
@@ -4,25 +4,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // models
 import 'package:english_app/models/main_model.dart';
+// details
+import 'package:english_app/details/rounded_button.dart';
+import 'package:english_app/details/rounded_text_field.dart';
+// constants
+import 'package:english_app/constants/strings.dart';
 
 class ProfileScreen extends ConsumerWidget {
-  const ProfileScreen({
-    Key? key,
-    required this.mainModel
-  }) : super(key: key);
+  const ProfileScreen({Key? key, required this.mainModel}) : super(key: key);
   final MainModel mainModel;
-  @override 
-  Widget build(BuildContext context,WidgetRef ref) {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Container(
-          child: Text(
-            mainModel.firestoreUser.email,
-            style: const TextStyle(fontSize: 40),
+          Text(
+            ("あなたのIDは${mainModel.firestoreUser.uid}です"),
+            style: const TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
           ),
-        ),
+          RoundedButton(
+              onPressed: () async => await mainModel.logout(
+                  context: context, mainModel: mainModel),
+              widthRate: 0.85,
+              color: Colors.red,
+              text: logoutText),
       ],
     );
   }
