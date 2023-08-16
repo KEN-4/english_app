7f1618470782bfc8d6374c41daeca5a123d167e8
KEN-4 <cuuw1561@icloud.com>
2023-08-16 11:29:20 +0900

    Update home_screen.dart

--- lib/views/main/home_screen.dart
+++ lib/views/main/home_screen.dart
@@ -10,31 +10,38 @@ import 'package:english_app/details/rounded_button.dart';
 // import 'package:english_app/details/rounded_text_field.dart';
 // constants
 import 'package:english_app/constants/strings.dart';
+// QuestionScreen
+import 'package:english_app/views/question_page.dart'; 
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key, required this.mainModel}) : super(key: key);
   final MainModel mainModel;
+  
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          ("英語力を測ろう"),
-          style: const TextStyle(fontSize: 30),
-          textAlign: TextAlign.center,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              ("英語力を測ろう"),
+              style: const TextStyle(fontSize: 30),
+              textAlign: TextAlign.center,
+            ),
+            RoundedButton(
+              onPressed: () {
+                // 問題ページへの遷移
+                Navigator.push(context, MaterialPageRoute(builder: (context) => QuestionPage()));
+              },
+              widthRate: 0.85,
+              color: Colors.blue,
+              text: "診断開始"  // クイズ開始ボタンのテキスト
+            ),
+          ],
         ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const QuestionPage()),
-            );
-          },
-          child: Text("Go to New Page"),
-        )
-      ],
+      ),
     );
   }
 }
